be71652df56fb81ddee7d079b860f80536b16c3c
kirillivanov <mail@kirillivanov.ru>
2012-05-26 08:10:58 +0400

    cleaning

--- app/controllers/form_builders_controller.rb
+++ app/controllers/form_builders_controller.rb
@@ -1,86 +1,64 @@
 class FormBuildersController < ApplicationController
-  # GET /form_builders
-  # GET /form_builders.json
-  
+
    before_filter :authenticate_user!
 
   def index
     @form_builders = FormBuilder.all
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @form_builders }
+      format.html
     end
   end
 
-  # GET /form_builders/1
-  # GET /form_builders/1.json
   def show
     @form_builder = FormBuilder.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @form_builder }
+      format.html
     end
   end
 
-  # GET /form_builders/new
-  # GET /form_builders/new.json
   def new
     @form_builder = FormBuilder.new
-    #@form_builder.build_form
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @form_builder }
+      format.html
     end
   end
 
-  # GET /form_builders/1/edit
   def edit
     @form_builder = FormBuilder.find(params[:id])
   end
 
-  # POST /form_builders
-  # POST /form_builders.json
   def create
     @form_builder = FormBuilder.new(params[:form_builder])
 
     respond_to do |format|
       if @form_builder.save
         format.html { redirect_to @form_builder, notice: 'Form builder was successfully created.' }
-        format.json { render json: @form_builder, status: :created, location: @form_builder }
       else
         format.html { render action: "new" }
-        format.json { render json: @form_builder.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /form_builders/1
-  # PUT /form_builders/1.json
   def update
     @form_builder = FormBuilder.find(params[:id])
-
     respond_to do |format|
       if @form_builder.update_attributes(params[:form_builder])
         format.html { redirect_to @form_builder, notice: 'Form builder was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @form_builder.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /form_builders/1
-  # DELETE /form_builders/1.json
+
   def destroy
     @form_builder = FormBuilder.find(params[:id])
     @form_builder.destroy
 
     respond_to do |format|
       format.html { redirect_to form_builders_url }
-      format.json { head :no_content }
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+  # devise overrides
   def resource_name
     :user
   end
@@ -12,24 +13,25 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  #appication logic
   def insert_form(name)
-    _meta = get_form_meta name
-    simple_form_for( resursify(_meta.resourse.name).new, :html => { :class => 'form-horizontal span7' }) do |form|
-      _meta.fields.visible.each_with_index do |field, i|
+    meta = get_form_meta name
+    simple_form_for(resursify(meta.resourse.name).new, html: { class: 'form-horizontal span7' }) do |form|
+      meta.fields.visible.each_with_index do |field, i|
         concat field_recognize(form, field)
       end
-      concat content_tag(:div, form.button(:submit, :class => 'btn btn-primary'), :class => "form-actions")
+      concat content_tag(:div, form.button(:submit, class: 'btn btn-primary'), class: 'form-actions')
     end
   end
 
   def get_list_of_models
     Dir[Rails.root.to_s + '/app/models/**/*.rb'].each { |file| require file }
     models = ActiveRecord::Base.subclasses.collect { |type| type.name }.sort
-    models-["User", "Resourse", "Column", "Field", "Option", "FormBuilder"]
+    models-%w(User Resourse Column Field Option FormBuilder)
   end
 
   def get_columns(resource)
-    resource.column_names-['id', 'created_at', 'updated_at']
+    resource.column_names-%w(id created_at updated_at)
   end
 
   def resursify(name)
@@ -37,21 +39,21 @@ module ApplicationHelper
   end
 
   private 
-
   def get_form_meta(name) 
-    FormBuilder.find_by_name(name)
+    FormBuilder.find_by_name name
   end
 
   def field_recognize(f, field)
-    if field.variant == 'text_field'
-      f.input field.column.name, label: field.label, :as => :string, :input_html => { :value => "#{ field.options.first.value unless field.options.empty? }"}
-    elsif field.variant == 'date_field'
-      f.input field.column.name, :as => :string, :input_html => { :value => "#{ field.options.first.value unless field.options.empty? }", :class => "datepicker" }
-    elsif field.variant == 'select_field'
-      f.input field.column.name, :as => :select, collection: field.options.map { |o| o.value if o.value }, include_blank: false
-    elsif field.variant == 'number_field' 
-      f.input field.column.name, :as => :integer, :input_html =>{ :value => "#{ field.options.first.value unless field.options.empty? }"}  
-    end
+    field = case field.variant
+            when 'text_field'
+              f.input field.column.name, label: field.label, as: :string, input_html: { value: "#{ field.options.first.value unless field.options.empty? }" }
+            when 'date_field'
+              f.input field.column.name, as: :string, input_html: { value: "#{ field.options.first.value unless field.options.empty? }", class: 'datepicker' }
+            when 'select_field'
+              f.input field.column.name, as: :select, collection: field.options.map { |o| o.value if o.value }, include_blank: false
+            when 'number_field' 
+              f.input field.column.name, as: :integer, input_html: { value: "#{ field.options.first.value unless field.options.empty? }" }  
+            end
   end
 end
 
