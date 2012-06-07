0e6df90f62aa335fd4d94d880aa08ffd55054771
kirillivanov <mail@kirillivanov.ru>
2012-06-07 22:13:26 +0400

    cleanup

--- spec/models/Resourse.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "ResourseSpec" do
-  it "should manage columns" do
-    column = FactoryGirl.create :column
-    resourse = column.resourse
-    resourse.can_manage_column?(column).should be_true
-  end
-end 
